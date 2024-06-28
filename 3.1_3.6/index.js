bb0b4bd718db586c03a0f5e4d365285ccf0c3bd8
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-06-28 16:30:32 +0200

    3.8

--- 3.1_3.6/index.js
+++ 3.1_3.6/index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan');
 
-app.use(morgan('tiny'));
 
 app.use(express.json()) 
+app.use(morgan('tiny'));
+
+
+morgan.token('post-data', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
+
 
 let persons = [
     { 
