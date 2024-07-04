5dd15adaa4dc6e218a7f20ea3a87dcb9cf3ca8bc
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2024-07-04 19:21:19 +0200

    3.12

---
+++ backend/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@clusterfso.z5mjknl.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=ClusterFSO`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  //Since the parameter is an empty object{} we get all of the notes stored in the notes collection.
+  Person.find({}).then(result => { 
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+
+
+//Person.find({ important: true }).then(result => {
+  // ...})
+}else{
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
+
+  const person = new Person({
+  name: newName,
+  number: newNumber,
+  })
+
+
+  person.save().then(result => {
+  console.log(`added ${newName} number ${newNumber} to phonebook`)
+  mongoose.connection.close()
+  })
+}
